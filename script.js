1018684ac3cd52ac3c557caf56feaba827e851ec
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-19 12:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+let rotation = 0;
 
-var line = document.getElementById("line");
-var ctx = line.getContext('2d');
+function rotateLine() {
+  rotation += 1;
+  line.style.transform = `rotate(${rotation}deg)`;
+}
 
-    // Set the center point and initial angle
-    var centerX = line.width / 2;
-    var centerY = line.height / 2;
-    var angle = 0;
-
-function rotateLine(){
-      // Clear the line
-      ctx.clearRect(0, 0, line.width, line.height);
-
-      // Update the angle for rotation
-      angle += 0.01;
-
-      // Calculate the line endpoints
-      var lineLength = 100;
-      var x1 = centerX - lineLength / 2;
-      var y1 = centerY;
-      var x2 = centerX + lineLength / 2;
-      var y2 = centerY;
-
-      // Rotate the line around the center point
-      ctx.translate(centerX, centerY);
-      ctx.rotate(angle);
-      ctx.translate(-centerX, -centerY);
-
-      // Draw the line
-      ctx.beginPath();
-      ctx.moveTo(x1, y1);
-      ctx.lineTo(x2, y2);
-      ctx.stroke();
-
-      // Reset the transformations
-      ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-      // Request animation frame to create a smooth animation
-      requestAnimationFrame(drawLine);
-    }
+setInterval(rotateLine, 10);
 
     // Start the animation
     rotateLine();
